88ceb25d9ab37aa73464044ab244f0f51ba19ed5
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2020-12-17 17:43:04 -0800

    confirmed cases

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ getStats()
 function getStates(state){
   state = state[0].toUpperCase() + state.slice(1)
   const {date, confirmed, deaths} = STATS[state][STATS[state].length -1]
-    console.log(`${date} active cases: ${confirmed} deaths: ${deaths}`)
-    return `${date} active cases: ${confirmed} deaths: ${deaths}`
+    console.log(`Since ${date} there has been ${confirmed} active cases and ${deaths} deaths caused by COVID-19 in ${state}.`)
+    return `Since ${date} there has been ${confirmed} active cases and ${deaths} deaths caused by COVID-19 in ${state}.`
 }
 
 fetch('https://energ.ee/covid19-us-api/states.json')
