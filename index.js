dfe381efa6ffcd4259c472162c0cc977aa001c6d
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2020-12-18 19:54:14 -0800

    submit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Confirmed COVID-19 Cases in each state
 // function getConfirmedCases(state) {
 let STATS = null
+let START = $('.start')
 function getStats() {
   fetch('https://energ.ee/covid19-us-api/states.json')
   .then(response => response.json())
@@ -13,10 +14,12 @@ function getStats() {
 getStats() 
 
 function getStates(state){
+  const num = new Intl.NumberFormat()
+  if(!state) return
   state = state[0].toUpperCase() + state.slice(1)
   const {date, confirmed, deaths} = STATS[state][STATS[state].length -1]
     console.log(`Since ${date} there has been ${confirmed} confirmed cases and ${deaths} deaths caused by COVID-19 in ${state}.`)
-    return `<h3> Since ${date} there has been ${confirmed} confirmed cases and ${deaths} deaths caused by COVID-19 in ${state}.</h3>`
+    return `<h3> Since ${new Date(date).toLocaleDateString()} there has been ${num.format(confirmed)} confirmed cases and ${num.format(deaths)} deaths caused by COVID-19 in ${state}.</h3>`
 }
 
 fetch('https://energ.ee/covid19-us-api/states.json')
@@ -31,16 +34,22 @@ fetch('https://energ.ee/covid19-us-api/states.json')
       const currentState = Object.keys(stateData)[0]
       caseObj[currentState] = stateData[currentState][stateData[currentState].length -1]
     })
-    console.log(caseObj)
+    // console.log(caseObj)
   })
 
 // COVID-19 testing sites
 function getLocation(state) {
-  fetch(`https://covid-19-testing.github.io/locations/${state}/complete.json`)
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson, state))
-    .catch(error => console.log(error));
+  if(!state) {
+    console.log('clear')
+    displayResults()
+    $('.results').html(START)
+  } else {
+    fetch(`https://covid-19-testing.github.io/locations/${state}/complete.json`)
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResults(responseJson, state))
+      .catch(error => console.log(error));
+  }
 }
 
 function getWeekday(number) {
@@ -86,21 +95,20 @@ function displayAddress(address) {
   return html
 }
 
-function displayResults(responseJson, state) {
+function displayResults(responseJson=[], state='') {
   let html = getStates(state)
   html += '<ul>'
   responseJson.forEach(location => {
-    icons(location.transportation)
-    if (location || location.phones || location.phones.length > 0) { return ""}
+    // icons(location.transportation)
       html += `
       <li>
          <h2>${location.name}</h2>
-         <p>${location.description}</p>
+         <p class="description">${location.description}</p>
          ${displayAddress(location.physical_address[0])}
-         ${location.phones[0] ? `<p>Telephone number: <a href="tel:${location.phones[0].number}">${location.phones[0].number}</a></p>` : "" }
-         <p>Language: ${location.phones[0].language ? location.phones[0].language : ""} </p>
+         ${location.phones[0] ? `<p>☎️: <a href="tel:${location.phones[0].number}">${location.phones[0].number}</a></p>` : "" }
+         <p>Language: ${location.phones[0] ? location.phones[0].language : ""} </p>
          ${location.regular_schedule ? displaySchedule(location.regular_schedule) : ""}
-         <p>${location.transportation ? location.transportation : ""}</p>
+         <p>${location.transportation ? icons(location.transportation) : ""}</p>
          </li>`
         })
         html += '</ul>'
@@ -110,15 +118,29 @@ function displayResults(responseJson, state) {
 //(location.phones[0].number == null || location.phones[0].language == null || location.regular_schedule == null || location.transportation == null ) { return ""}
      
 function icons(str) {
-    let iconArray = str.split(',')
+    let obj = {
+      Car: "🚗",
+      Bus: "🚌",
+      Uber: "🚕",
+      Shuttle: "🚐",
+      "DriveThrough": "🚘",
+      Depends: "❓",
+      Train: "🚆"
+    }
+    let iconArray = str.replaceAll(' ','').split(',')
+
+    let result = iconArray.map(icon => {
+      return `${obj[icon]} ${icon}`
+    })
+    console.log(result)
     console.log(iconArray)
+    return result.join(', ')
 }
 
 function watchForm() {
   $('select').change(event => {
     event.preventDefault();
     let state = $('select :selected').val()
-    console.log(state)
     getLocation(state);
   });
 }
